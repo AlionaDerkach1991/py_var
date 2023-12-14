66b43573141fb815a88990746fd00d0032dce8cf
Aliona <aderkach1991@gmail.com>
2023-12-14 11:11:59 +0200

    var/hm/ex_1 done

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print(34)
+try:
+    number1 = 5
+    number2 = 15
+    number3 = 25
+    print(number1 + number2 + number3)
+    print(number1 * number2 * number3)
+except Exception as e:
+    print(e)
