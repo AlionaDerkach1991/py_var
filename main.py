f1ef2556603ca09ed156520f152b99f06d4c2a5f
Aliona <aderkach1991@gmail.com>
2023-12-14 11:37:15 +0200

    var/hm/ex_3 done

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(34)
+try:
+    print('Діагональ1 - number1 = 24')
+    print('Діагональ2 - number2 = 18')
+    print('Площа ромба')
+    number1 = 24
+    number2 = 18
+    print(number1*number2/2)
+except Exception as e:
+    print(e)
