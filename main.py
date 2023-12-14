f72d9d975ef26abe37a62312da93140f0a831bb7
Aliona <aderkach1991@gmail.com>
2023-12-14 11:00:54 +0200

    var/lab/ex_3 done

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(34)
+try:
+    print('Катет1 - number1 = 8')
+    print('Катет2 - number2 = 6')
+    print('Площа трикутника')
+    number1 = 8
+    number2 = 6
+    print(number1*number2/2)
+except Exception as e:
+    print(e)
