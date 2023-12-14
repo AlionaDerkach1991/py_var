088f4bf1e37b6eb1ef6a7826836e8ee5fb03b12a
Aliona <aderkach1991@gmail.com>
2023-12-14 11:29:39 +0200

    var/hm/ex_2 done

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(34)
+try:
+    print('Зарплата за місяць - number1 = 20000')
+    print('Сума місячного платежу за кредитом у банку - number2 = 3000')
+    print('Заборгованість за комунальні послуги - number3 = 2500')
+    print('Залишок коштів на власні потреби:')
+    number1 = 20000
+    number2 = 3000
+    number3 = 2500
+    print(number1 - number2 - number3)
+except Exception as e:
+    print(e)
